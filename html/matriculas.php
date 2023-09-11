28f3bfd6fd7d90edd6c9798bcb972e4b754fe707
mavizokz <130993062+mavizokz@users.noreply.github.com>
2023-09-11 13:23:48 -0300

    atualizando o php
    atualizei o código php
    mexi no formulário de currículo  e no slider da matricula

--- html/matriculas.php
+++ html/matriculas.php
@@ -1,17 +1,28 @@
 <?php 
-$para ="honoriomariavitoria3@gmail.com"; 
-
 $nome = $_POST['nomedoresponsavel'];
-echo "é um prazer te conhecer"
-
 $nomedofilho = $_POST['nomedofilho'];
-
 $email = $_POST['email'];
-
 $celular = $_POST['celular'];
-
 $mensagem = $_POST['serie'];
+$unidade = $_POST['unidade'];
 
-$unidade = $_POST['unidade']
+echo "<p> É um prazer te conhecer $nome!</p>";
 
+$arquivo = "Nome: $nome , E-mail: $email";
+
+$destino ="maria.honorio1511@gmail.com"; 
+ 
+//Este sempre deverá existir para garantir a exibição correta dos caracteres
+$headers  = "MIME-Version: 1.0\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+$headers .= "From: $nome <$email>\n";
+$headers .= "Celular: $celular\n";
+$headers .= "Mensagem: $mensagem";
+
+
+if (mail($destino, $arquivo, $headers)) {
+    echo "Email enviado com sucesso";
+} else{
+    echo "Falha ao enviar Email";
+};
 ?>
